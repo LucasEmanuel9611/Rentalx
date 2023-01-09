47338d83ebbdc0386564a90c1bbd0193692f6961
LucasEmanuel9611 <lucas190717@gmail.com>
2023-01-09 09:47:02 -0300

    feat: better post response with using json

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,16 +1,24 @@
 import { Router } from "express";
-import { v4 as uuidv4 } from "uuid";
+import { Category } from "../model/Category";
 
 export const categoriesRoutes = Router();
 
-let categories = [];
+let categories: Category[] = [];
 
 categoriesRoutes.post("/categories", (req, res) => {
   const { name, description } = req.body;
 
-  const category = { name, description, id: uuidv4() };
+  const category = new Category();
+
+  Object.assign(category, {
+    name,
+    description,
+    created_at: new Date(),
+  });
 
   categories.push(category);
 
-  return res.status(201).send();
+  return res.status(201).json({
+    category,
+  });
 });
