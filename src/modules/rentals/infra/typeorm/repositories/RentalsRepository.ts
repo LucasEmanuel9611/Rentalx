f7eed56828bc32a85e2625975cac2178cc8ab207
lucasEmanuel9611 <lucas190717@gmail.com>
2023-05-04 21:28:26 -0300

    fix: create Rental verifications

--- src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
+++ src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
@@ -11,12 +11,16 @@ export class RentalsRepository implements IRentalsRepository {
   }
 
   async findOpenRentalByCar(car_id: string): Promise<Rental> {
-    const openByCar = await this.repository.findOne({ car_id });
+    const openByCar = await this.repository.findOne({
+      where: { car_id, end_date: null },
+    });
     return openByCar;
   }
 
   async findOpenRentalByUser(user_id: string): Promise<Rental> {
-    const openByUser = await this.repository.findOne({ user_id });
+    const openByUser = await this.repository.findOne({
+      where: { user_id, end_date: null },
+    });
     return openByUser;
   }
 
@@ -25,6 +29,11 @@ export class RentalsRepository implements IRentalsRepository {
 
     await this.repository.save(rental);
 
+    return rental;
+  }
+
+  async findById(id: string): Promise<Rental> {
+    const rental = await this.repository.findOne(id);
     return rental;
   }
 }
