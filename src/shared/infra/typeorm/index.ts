5da4294b5743a9ffaadf4474908af0fab7f5f7f9
Lucas Emanuel <lucas190717@gmail.com>
2023-05-16 13:28:05 -0300

    refactor: configure typeorm for production

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,11 +1,10 @@
 import { Connection, createConnection, getConnectionOptions } from "typeorm";
 
-export default async (host = "database"): Promise<Connection> => {
+export default async (): Promise<Connection> => {
   const defaultConnection = await getConnectionOptions();
 
   return createConnection(
     Object.assign(defaultConnection, {
-      host: process.env.NODE_ENV === "test" ? "localhost" : host,
       database:
         process.env.NODE_ENV === "test"
           ? "rentx_test"
